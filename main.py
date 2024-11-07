1ace7c6fcf3d6ba95afdfe49a3271ec8a8961d5a
jayrad03 <jahredglover@gmail.com>
2024-11-07 14:00:38 +1100

    pritning

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def select_face_option(event):
 
 def roll_all_dice(event):
 
-    dice_type = int(document.getElementById("Coin"))
-    diceA = int(document.getElementById("DiceAmount"))
+    dice_type = document.getElementById("Coin")
+    diceA = document.getElementById("DiceAmount")
+    print(dice_type)
+    print(diceA)
     #dice.dice_roll(dice_type)
 
     for roll in range(diceA):
