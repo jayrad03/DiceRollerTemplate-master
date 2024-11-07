d4784ca200eaa1541186c733e31880fbb095a84c
jayrad03 <jahredglover@gmail.com>
2024-11-07 13:01:25 +1100

    debug

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import dice
 # this stores the selected face option from the drop-down list
 dice_type = "Coin"
 diceA = "DiceAmount"
-button = document.getElementById("rollButton").addEventListener("click", roll_all_dice)
+
 
 def select_face_option(event):
     global dice_type  # use global var named dice_type
@@ -27,3 +27,5 @@ def roll_all_dice(event):
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
     document.querySelector("div#roll-history").innerHTML = ""
+    
+button = document.getElementById("rollButton").addEventListener("click", roll_all_dice)
