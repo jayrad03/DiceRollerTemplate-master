4b2437a1e517aa3114c748203114849a58b28b15
jayrad03 <jahredglover@gmail.com>
2024-11-07 14:02:20 +1100

    v

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def select_face_option(event):
 
 def roll_all_dice(event):
 
-    dice_type = document.getElementById("Coin")
-    diceA = document.getElementById("DiceAmount")
+    dice_type = document.getElementById("Coin.value")
+    diceA = document.getElementById("diceAmount")
     print(dice_type)
     print(diceA)
     #dice.dice_roll(dice_type)
